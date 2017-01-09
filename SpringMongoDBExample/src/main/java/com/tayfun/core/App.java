fec1328825d3e018bb1332be7f5b21c0751affa2
tyfnckgz <tayfun.ackgz@gmail.com>
2017-01-09 15:27:48 +0300

    SpringMongoDBExample
    In this project you will learn how to use  MongoDB in a spring project

---
+++ SpringMongoDBExample/src/main/java/com/tayfun/core/App.java
@@ -0,0 +1,62 @@
+package com.tayfun.core;
+
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+
+import com.tayfun.config.SpringMongoConfig;
+import com.tayfun.model.User;
+//import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		// For XML
+		//ApplicationContext ctx = new GenericXmlApplicationContext("SpringConfig.xml");
+
+		// For Annotation
+		ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringMongoConfig.class);
+		MongoOperations mongoOperation = (MongoOperations) ctx.getBean("mongoTemplate");
+
+		User user = new User("ahmet", "pass1");
+
+		// save
+		mongoOperation.save(user);
+
+		// now user object got the created id.
+		System.out.println("1. user : " + user);
+		List<User> listUser = mongoOperation.findAll(User.class);
+		System.out.println("4. Number of user = " + listUser.size());
+		// query to search user
+		Query searchUserQuery = new Query(Criteria.where("username").is("ads"));
+
+		// find the saved user again.
+		User savedUser = mongoOperation.findOne(searchUserQuery, User.class);
+		System.out.println("2. find - savedUser : " + savedUser);
+
+		// update password
+		mongoOperation.updateFirst(searchUserQuery, Update.update("password", "pass2"),
+				User.class);
+
+		// find the updated user object
+		User updatedUser = mongoOperation.findOne(
+				new Query(Criteria.where("username").is("tayfun")), User.class);
+
+		System.out.println("3. updatedUser : " + updatedUser);
+
+		// delete
+//		mongoOperation.remove(searchUserQuery, User.class);
+
+		// List, it should be empty now.
+		
+		System.out.println("4. Number of user = " + listUser.size());
+
+	}
+
+}
